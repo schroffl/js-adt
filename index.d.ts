26971303fc16150351b3ae54b298346d80166a20
schroffl <schroffl@users.noreply.github.com>
2019-08-12 11:45:30 +0200

    Fix typo

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,6 @@ export function create(...ctors: string[]) : any
 export function when<R>(ctor: any, fn: (v: any) => R) : Matcher<R>
 export function match<R>(matchers: Matcher<R>[]) : MatchFn<R>
 export function is(type: any, inst: any) : boolean
-export function stringiyf(instance: any, indendation?: number) : string
+export function stringify(instance: any, indendation?: number) : string
 
 export const otherwise: any
