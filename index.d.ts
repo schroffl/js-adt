061d30fb16935a78927ac198db86c5243d1967fa
schroffl <schroffl@users.noreply.github.com>
2019-08-12 11:04:35 +0200

    Add typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+type Matcher<R> = { ctor: any, fn: (v: any) => R }
+type MatchFn<R> = (inst: any) => R
+
+export function create(...ctors: string[]) : any
+export function when<R>(ctor: any, fn: (v: any) => R) : Matcher<R>
+export function match<R>(matchers: Matcher<R>[]) : MatchFn<R>
+export function is(type: any, inst: any) : boolean
+
+export const otherwise: any
