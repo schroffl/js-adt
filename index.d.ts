97f2dd8836ffa5c721f7114c2d1be230487cee0b
schroffl <schroffl@users.noreply.github.com>
2019-08-14 14:18:57 +0200

    Make ctor argument optional

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 type Matcher<R> = { ctor: any, fn: (v: any) => R }
 type MatchFn<R> = (inst: any) => R
 
-export function create<T extends string>(...ctors: T[]) : { [K in T]: (value: any) => any }
+export function create<T extends string>(...ctors: T[]) : { [K in T]: (value?: any) => any }
 
 export function when<R>(ctor: any, fn: (v: any) => R) : Matcher<R>
 export function match<R>(matchers: Matcher<R>[]) : MatchFn<R>
