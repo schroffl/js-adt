94e08534ac5fe180ed73bd04e831a9c15bb6ab32
schroffl <schroffl@users.noreply.github.com>
2019-08-14 10:07:07 +0200

    Increase type safety

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,8 @@
 type Matcher<R> = { ctor: any, fn: (v: any) => R }
 type MatchFn<R> = (inst: any) => R
 
-export function create(...ctors: string[]) : any
+export function create<T extends string>(...ctors: T[]) : { [K in T]: (value: any) => any }
+
 export function when<R>(ctor: any, fn: (v: any) => R) : Matcher<R>
 export function match<R>(matchers: Matcher<R>[]) : MatchFn<R>
 export function is(type: any, inst: any) : boolean
